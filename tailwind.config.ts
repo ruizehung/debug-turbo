0eddcb877fa75a0af0125ddb6f9e52f4bbd33940
Ray Hung <howard88tw@gmail.com>
2024-11-12 21:17:00 -0500

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: [
+    "./src/**/*.tsx",
+    "./node_modules/preline/preline.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("preline/preline"),
+  ],
+} satisfies Config;
